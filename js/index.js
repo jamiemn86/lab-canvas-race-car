8fe802a4e4eef1f7643a2d57a1765e122f90bd53
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-04 21:21:01 +0200

    Iteration one working

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-  function startGame() {}
 };
+
+function startGame() {
+  const img = new Image();
+  img.src = '/images/road.png';
+  const backgroundImage = {
+    img: img,
+    x: 0,
+    draw: function () {
+      ctx.drawImage(this.img, this.x, 0);
+    }
+  };
+  backgroundImage.draw();
+  requestAnimationFrame(startGame);
+}
