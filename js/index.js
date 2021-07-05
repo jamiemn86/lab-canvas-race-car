c7ced22d2daa6418c6564b4cd539556b98febafe
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-05 21:21:42 +0200

    Up to and including iteration 3 completed, beginning iteration 4

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Iteration 1
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -5,6 +7,7 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
     drawCar();
+    drawObstacle();
   };
 };
 
@@ -26,6 +29,8 @@ function loadBackground() {
   requestAnimationFrame(loadBackground);
 }
 
+// Iteration 2
+
 const carImg = new Image();
 
 carImg.src = '/images/car.png';
@@ -45,6 +50,8 @@ function drawCar() {
   carOnScreen.draw();
   requestAnimationFrame(drawCar);
 }
+
+// Iteration 3
 
 function moveUp() {
   car.y -= 5;
@@ -83,3 +90,25 @@ function enableControls() {
 }
 
 enableControls();
+
+// Iteration 4
+
+class Obstacle {
+  constructor(x, y, width, height) {
+    (this.x = x), (this.y = y), (this.width = width), (this.height = height);
+  }
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function drawObstacle() {
+  const testObstacle = new Obstacle();
+  testObstacle.x = 115;
+  testObstacle.y = 0;
+  testObstacle.width = 80;
+  testObstacle.height = 10;
+  testObstacle.draw();
+  requestAnimationFrame(drawObstacle);
+}
